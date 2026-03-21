642dc3cb94e53d90047dc37d9078d77fd52f5e24
minhnhat166 <minhnhatphan2003@gmail.com>
2026-03-22 00:17:24 +0700

    refactor(ui): adjust split view responsiveness and scroll containers

--- src/pages/dashboard/employees/index.tsx
+++ src/pages/dashboard/employees/index.tsx
@@ -66,7 +66,7 @@ export default function EmployeesPage() {
 	return (
 		<DashboardSplitView
 			isSidebarCollapsed={isCollapsed}
-			sidebarClassName={isCollapsed ? "lg:w-14 xl:w-14" : "lg:w-[16rem] xl:w-1/5"}
+			sidebarClassName={isCollapsed ? "md:w-14 xl:w-14" : "md:w-[16rem] xl:w-1/5"}
 			sidebar={
 				<EmployeeSidebar
 					activeEmployeeId={activeEmployee?.id || null}
--- src/pages/dashboard/vendors/index.tsx
+++ src/pages/dashboard/vendors/index.tsx
@@ -67,7 +67,7 @@ export default function VendorsPage() {
 	return (
 		<DashboardSplitView
 			isSidebarCollapsed={isCollapsed}
-			sidebarClassName={isCollapsed ? "lg:w-14 xl:w-14" : "lg:w-[16rem] xl:w-1/5"}
+			sidebarClassName={isCollapsed ? "md:w-14 xl:w-14" : "md:w-[16rem] xl:w-1/5"}
 			sidebar={
 				<VendorSidebar
 					activeVendorId={activeVendorId}
